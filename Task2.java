747db0afe04ee5ad8d4e4f6f4ee299af6d4f8053
Divyani <divyanikhatri2007@gmail.com>
2025-12-26 10:19:02 +0545

    Updated utils class

--- Task2.java
+++ Task2.java
@@ -3,6 +3,8 @@ package tuto7;
 public class Task2 {
 	public static void greet( ) {
 		System.out.println("Hello I am Divyani Don");
+		System.out.println("Update utils from branch A");
+		
 	}
 
 }
