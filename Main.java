812ae279b567de968cd6e56107e14624a731996b
Divyani <divyanikhatri2007@gmail.com>
2026-01-04 22:34:04 +0545

    main task completed

---
+++ Main.java
@@ -0,0 +1,19 @@
+package tuto7;
+
+/**
+ * the main class serves as an alternative entry point for the application.
+ * Currently unused.
+ */
+public class Main {
+
+	//default constructor
+	/**
+	 * main method - currently not implemented.
+	 * 
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
